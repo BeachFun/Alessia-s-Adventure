6855f81bc1c1245645c5f57ac634948e6ea8a937
PROG MISTERIO <habib2004habib@yandex.ru>
2023-08-11 16:12:57 +0300

    Скрипты контроллеры врагов
    Создал скрипты для каждого врага, которые будут контролировать поведение врагов.

---
+++ Assets/CORE/Code/Scripts/Enemy2Controller.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Animator))]
+
+public class Enemy2Controller : MonoBehaviour
+{
+    [SerializeField] private Animator animator;
+
+    private float speed = 0f;
+
+    private void Start()
+    {
+        animator = GetComponent<Animator>();
+        StartCoroutine(Loop());
+    }
+
+    private IEnumerator Loop()
+    {
+        yield return null;
+
+    }
+}
