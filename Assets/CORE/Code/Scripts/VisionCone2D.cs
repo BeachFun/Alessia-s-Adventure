40996ecb14853d9089078b89142ee7659782bebd
PROG MISTERIO <habib2004habib@yandex.ru>
2023-09-11 18:53:25 +0300

    Улучшение скрипта VisionCone2D
    1. Сделал поля приватными и сериализуемыми, которые должны быть видны в инспекторе.
    2. Добавил новую функцию "Обнаружение множества объектов", если она включена, то скрипт будет сохранять ссылку сразу на несколько обнаруженных объектов.
    3. Добавил функцию включения/отключения зрения

--- Assets/CORE/Code/Scripts/VisionCone2D.cs
+++ Assets/CORE/Code/Scripts/VisionCone2D.cs
@@ -1,15 +1,26 @@
+using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 
 public class VisionCone2D : MonoBehaviour
 {
-    public float visionAngle = 60.0f;
-    public float maxDistance = 10.0f;
-    public LayerMask targetLayer;
-    public Transform eyesTransform;
+    [SerializeField] private float visionAngle = 60.0f;
+    [SerializeField] private float maxDistance = 10.0f;
+    [SerializeField] private LayerMask targetLayer;
+    [SerializeField] private Transform eyesTransform;
+    [SerializeField] private bool detectMultipleObjects = false;
+    [SerializeField] private bool shouldCheckVision = false;
+
+    private List<GameObject> _detectedObjects;
+
+    public List<GameObject> DetectedObjects
+    {
+        get => _detectedObjects;
+    }
 
     private void Update()
     {
-        CheckVision();
+        if (shouldCheckVision) CheckVision();
     }
 
     public void CheckVision()
@@ -18,18 +29,31 @@ public class VisionCone2D : MonoBehaviour
         Vector2 direction = eyesTransform.right;
         float halfAngle = visionAngle / 2.0f;
 
-        for (float angle = -halfAngle; angle <= halfAngle; angle += 1.0f)
-        {
-            Vector2 rayDirection = Quaternion.Euler(0, 0, angle) * direction;
+        _detectedObjects.Clear();
 
-            RaycastHit2D hit = Physics2D.Raycast(eyesTransform.position, rayDirection, maxDistance, targetLayer);
-
-            if (hit.collider != null)
+        if (detectMultipleObjects)
+            for (float angle = -halfAngle; angle <= halfAngle; angle += 1.0f)
             {
-                // Обработайте объект, который был обнаружен в зоне видимости
-                GameObject target = hit.collider.gameObject;
-                Debug.Log("Object " + target.name + " is in vision cone.");
+                Vector2 rayDirection = Quaternion.Euler(0, 0, angle) * direction;
+
+                RaycastHit2D hit = Physics2D.Raycast(eyesTransform.position, rayDirection, maxDistance, targetLayer);
+
+                if (hit.collider != null) _detectedObjects.Add(hit.collider.gameObject);
             }
-        }
+        else
+            for (float angle = -halfAngle; angle <= halfAngle; angle += 1.0f)
+            {
+                Vector2 rayDirection = Quaternion.Euler(0, 0, angle) * direction;
+
+                IEnumerable<GameObject> hits = Physics2D.RaycastAll(eyesTransform.position, rayDirection, maxDistance, targetLayer)
+                    .Where(e => e.collider != null)
+                    .Select(e => e.collider.gameObject);
+
+                if (hits.Count() > 0) _detectedObjects.AddRange(hits);
+            }
     }
+
+    public void VisionStart() => shouldCheckVision = true;
+
+    public void VisionEnd() => shouldCheckVision = false;
 }
