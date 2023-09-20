6e5648389cb1a46275ec2be4e57f0c0b8fe33649
PROG MISTERIO <habib2004habib@yandex.ru>
2023-09-20 11:16:04 +0300

    Update PatrolMovingAlEditor.cs

--- Assets/CORE/Code/Scripts/Editor/PatrolMovingAlEditor.cs
+++ Assets/CORE/Code/Scripts/Editor/PatrolMovingAlEditor.cs
@@ -9,7 +9,6 @@ public class PatrolMovingAlEditor : Editor
     private SerializedObject serializedMovingAl;
 
     private SerializedProperty isPaused;
-    private SerializedProperty useGravity;
     private SerializedProperty jumpForce;
     private SerializedProperty movementAlgorithm;
     private SerializedProperty isAvoidObstacles;
@@ -26,7 +25,6 @@ public class PatrolMovingAlEditor : Editor
         serializedMovingAl = new SerializedObject(movingAl);
 
         isPaused = serializedMovingAl.FindProperty("isPaused");
-        useGravity = serializedMovingAl.FindProperty("useGravity");
         jumpForce = serializedMovingAl.FindProperty("jumpForce");
         movementAlgorithm = serializedMovingAl.FindProperty("movementAlgorithm");
         isAvoidObstacles = serializedMovingAl.FindProperty("isAvoidObstacles");
@@ -71,13 +69,7 @@ public class PatrolMovingAlEditor : Editor
         EditorGUILayout.Space();
         EditorGUILayout.PropertyField(rotateSeconds, new GUIContent("Время разворота"));
         EditorGUILayout.PropertyField(moveSpeed, new GUIContent("Скорость перемещения"));
-
-        if (useGravity.boolValue)
-        {
-            EditorGUILayout.PropertyField(jumpForce, new GUIContent("Сила прыжка"));
-        }
-
-        EditorGUILayout.PropertyField(useGravity, new GUIContent("Гравитация"));
+        EditorGUILayout.PropertyField(jumpForce, new GUIContent("Сила прыжка"));
 
         if (serializedMovingAl.hasModifiedProperties)
         {
