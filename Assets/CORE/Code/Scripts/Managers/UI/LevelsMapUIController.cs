19a0946966533947126ffc210e9c3378a89f360e
PROG MISTERIO <habib2004habib@yandex.ru>
2023-09-23 09:04:53 +0300

    Карта выбора уровней
    1. Сцена выбора уровней
    2. Контроллер содержащий логику сцены

---
+++ Assets/CORE/Code/Scripts/Managers/UI/LevelsMapUIController.cs
@@ -0,0 +1,68 @@
+using System.Linq;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class LevelsMapUIController : MonoBehaviour
+{
+    [SerializeField] private string mainSceneName;
+    [SerializeField] private string[] levelsNames;
+    [Header("Location")]
+    [SerializeField] private Sprite[] locationSprites;
+    [Header("UI Elements")]
+    [SerializeField] private Image locationImage;
+    [SerializeField] private Button buttonLeft;
+    [SerializeField] private Button buttonRight;
+    [SerializeField] private GameObject[] groupListsLevels;
+
+    private int _currentLocation;
+
+
+    private void Start()
+    {
+        LocationChangeHandler();
+
+        locationImage.sprite = locationSprites[_currentLocation];
+    }
+
+    public void BackToMain()
+    {
+        SceneManager.LoadScene(mainSceneName);
+    }
+
+    public void OpenLevel(int levelNumber)
+    {
+        if (levelNumber > levelsNames.Length || levelNumber < 1) return;
+
+        SceneManager.LoadScene(levelsNames[levelNumber]);
+    }
+
+    public void LeftButtonOnClick()
+    {
+        if (_currentLocation > 0)
+        {
+            _currentLocation--;
+
+            LocationChangeHandler();
+        }
+    }
+
+    public void RightButtonOnClick()
+    {
+        if (_currentLocation < locationSprites.Length - 1)
+        {
+            _currentLocation++;
+
+            LocationChangeHandler();
+        }
+    }
+
+    private void LocationChangeHandler()
+    {
+        buttonLeft.gameObject.SetActive(_currentLocation > 0);
+        buttonRight.gameObject.SetActive(_currentLocation < locationSprites.Length - 1);
+
+        groupListsLevels.ToList().ForEach(e => e.SetActive(false));
+        groupListsLevels[_currentLocation].SetActive(true);
+    }
+}
