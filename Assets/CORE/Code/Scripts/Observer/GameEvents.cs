f5dbde6d96d8e3cdc23e483bd6cd7bb808c92ff7
PROG MISTERIO <habib2004habib@yandex.ru>
2023-09-25 14:55:35 +0300

    New class GameEvents
    Список наименований событий

---
+++ Assets/CORE/Code/Scripts/Observer/GameEvents.cs
@@ -0,0 +1,10 @@
+﻿internal class GameEvents
+{
+    public static string GAME_INDICATORS_STARTED { get; private set; } = "GAME_INDICATORS_STARTED";
+
+
+    public static string DIAMOND_CHANGED { get; private set; } = "DIAMOND_CHANGED";
+    public static string PLAYER_HEALTH_CHANGED { get; private set; } = "PLAYER_HEALTH_CHANGED";
+    public static string PLAYER_ENERGY_CHANGED { get; private set; } = "PLAYER_ENERGY_CHANGED";
+    public static string PLAYER_DAGGER_CHANGED { get; private set; } = "PLAYER_DAGGER_CHANGED";
+}
