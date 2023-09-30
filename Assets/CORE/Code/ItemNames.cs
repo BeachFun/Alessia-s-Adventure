850564e5002aa1daf47abd8392eedc54451acc36
PROG MISTERIO <habib2004habib@yandex.ru>
2023-09-30 11:51:39 +0300

    Create ItemNames.cs

---
+++ Assets/CORE/Code/ItemNames.cs
@@ -0,0 +1,7 @@
+﻿public class ItemNames
+{
+    internal static string Dagger { get; private set; } = "dagger";
+    internal static string Heart { get; private set; } = "heart";
+    internal static string SmallDiamond { get; private set; } = "diamond small";
+    internal static string BigDiamond { get; private set; } = "diamond big";
+}
