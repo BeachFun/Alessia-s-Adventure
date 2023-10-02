ff6d75fda4b5e8fb8dfc5ac2d6f89e66d614e1f0
PROG MISTERIO <habib2004habib@yandex.ru>
2023-10-02 09:48:54 +0300

    Update LevelData.cs
    Хранит еще больше данных и умеет объединяться с другим LevelData

--- Assets/CORE/Code/Scripts/Structures/LevelData.cs
+++ Assets/CORE/Code/Scripts/Structures/LevelData.cs
@@ -1,4 +1,5 @@
-﻿using UnityEngine;
+﻿using System;
+using UnityEngine;
 
 [System.Serializable]
 
@@ -7,32 +8,70 @@
 /// </summary>
 internal struct LevelData
 {
-    [SerializeField] private int level;
-    [SerializeField] private string name;
+    [SerializeField] private int levelIndex;
+    [SerializeField] private string levelName;
     [SerializeField] private string timeAccess;
     [SerializeField] private int enemiesIntoLevel;
+    [SerializeField] private int maxDiamonds;
 
     private bool isComplete;
     private string timeComplete;
     private int enemiesKillCounter;
     private int collectedDiamondCount;
+    private int bestScore;
 
 
-    internal LevelData(int levelIndex, string name)
+    internal LevelData(int levelIndex, string levelName)
     {
         this = new LevelData();
 
-        Level = levelIndex;
-        Name = name;
-        timeAccess = System.TimeSpan.Zero.ToString();
+        LevelIndex = levelIndex;
+        LevelName = levelName;
+        timeComplete = TimeSpan.Zero.ToString();
+        timeAccess = TimeSpan.Zero.ToString();
     }
 
-    public int Level { get => level; set => level = value; }
-    public string Name { get => name; set => name = value; }
-    public string TimeAccess { get => timeAccess; set => timeAccess = value; }
-    public int EnemiesIntoLevel { get => enemiesIntoLevel; set => enemiesIntoLevel = value; }
+    public int LevelIndex { get => levelIndex; set => levelIndex = value; }
+    public string LevelName { get => levelName; set => levelName = value; }
     public bool IsComplete { get => isComplete; set => isComplete = value; }
     public string TimeComplete { get => timeComplete; set => timeComplete = value; }
+    public string TimeAccess { get => timeAccess; set => timeAccess = value; }
     public int EnemiesKillCounter { get => enemiesKillCounter; set => enemiesKillCounter = value; }
+    public int EnemiesIntoLevel { get => enemiesIntoLevel; set => enemiesIntoLevel = value; }
     public int CollectedDiamondCount { get => collectedDiamondCount; set => collectedDiamondCount = value; }
+    public int MaxDiamonds { get => maxDiamonds; set => maxDiamonds = value; }
+    public int BestScore { get => bestScore; set => bestScore = value; }
+
+
+    internal LevelData Union(LevelData newLevelData)
+    {
+        if (newLevelData.levelIndex != levelIndex) throw new ArgumentException();
+
+        if (newLevelData.isComplete)
+        {
+            isComplete = newLevelData.isComplete;
+        }
+
+        if (timeComplete is null || TimeSpan.Parse(newLevelData.timeComplete) < TimeSpan.Parse(timeComplete))
+        {
+            timeComplete = newLevelData.timeComplete;
+        }
+
+        if (newLevelData.enemiesKillCounter > enemiesKillCounter)
+        {
+            enemiesKillCounter = newLevelData.enemiesKillCounter;
+        }
+
+        if (newLevelData.collectedDiamondCount > collectedDiamondCount)
+        {
+            collectedDiamondCount = newLevelData.collectedDiamondCount;
+        }
+
+        if (newLevelData.bestScore > bestScore)
+        {
+            bestScore = newLevelData.bestScore;
+        }
+
+        return this;
+    }
 }
