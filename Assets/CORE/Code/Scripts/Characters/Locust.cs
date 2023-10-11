a6c0886392c328a0cca3f02ee99df8661ed6b5ac
PROG MISTERIO <habib2004habib@yandex.ru>
2023-10-11 13:01:10 +0300

    Add new Enemy - Locust

--- Assets/CORE/Code/Scripts/Characters/Locust.cs
+++ Assets/CORE/Code/Scripts/Characters/Locust.cs
@@ -1,18 +1,74 @@
-using System.Collections;
-using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
+
+[RequireComponent(typeof(Animator))]
+[RequireComponent(typeof(PatrolMovingAl))]
+[RequireComponent(typeof(VisionCone2D))]
 
 public class Locust : Enemy
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField] private protected string playerTag = "Player";
+
+    private PatrolMovingAl _movingSystem;
+    private VisionCone2D _vision2D;
+
+
+    private protected override void Start()
     {
-        
+        base.Start();
+
+        _movingSystem = GetComponent<PatrolMovingAl>();
+        _vision2D = GetComponent<VisionCone2D>();
+
+        _attackSystem.ActionBeforeAttack += ActionBeforeAttackHandler;
+        _attackSystem.ActionAfterAttack += ActionAfterAttackHandler;
     }
 
     // Update is called once per frame
-    void Update()
+    private protected override void FixedUpdate()
     {
-        
+        base.FixedUpdate();
+
+        _vision2D.IsRotated = _spriteRenderer.flipX;
+        _vision2D.CheckVision();
+
+        Transform playerTransform = null;
+        if (_vision2D.DetectedObjects.Any(e => e.tag == playerTag))
+        {
+            playerTransform = _vision2D.DetectedObjects.First(e => e.tag == playerTag).transform;
+        }
+
+        if (playerTransform is not null)
+        {
+            _movingSystem.FastMoveOn = true;
+            _movingSystem.IsRotating = false;
+            _animator.speed = 1.3f;
+        }
+        else
+        {
+            _movingSystem.FastMoveOn = false;
+            _movingSystem.IsRotating = true;
+            _animator.speed = 1f;
+        }
+
+        _animator.SetFloat("speed", _movingSystem.Speed);
+    }
+
+    private void OnDestroy()
+    {
+        _attackSystem.ActionBeforeAttack -= ActionBeforeAttackHandler;
+        _attackSystem.ActionAfterAttack -= ActionAfterAttackHandler;
+    }
+
+    private void ActionBeforeAttackHandler()
+    {
+        _movingSystem.StopMovement(SnapAxis2D.X);
+        _movingSystem.Pause = true;
+        _animator.SetFloat("speed", 0);
+    }
+
+    private void ActionAfterAttackHandler()
+    {
+        _movingSystem.Pause = false;
     }
 }
