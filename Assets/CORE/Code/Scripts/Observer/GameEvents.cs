2e220463d531221207ce36ef34bd9ad4d6c983bd
PROG MISTERIO <habib2004habib@yandex.ru>
2023-10-11 10:21:32 +0300

    New StartupNotice and Gamev Events

--- Assets/CORE/Code/Scripts/Observer/GameEvents.cs
+++ Assets/CORE/Code/Scripts/Observer/GameEvents.cs
@@ -1,13 +1,14 @@
 ﻿internal class GameEvents
 {
-    public static string ALL_MANAGERS_STARTED { get; private set; } = "ALL_MANAGERS_STARTED";
-
-
-
     public static string LEVEL_IS_SELECTED_FOR_STARTED { get; private set; } = "LEVEL_IS_SELECTED_FOR_STARTED";
-    public static string LEVEL_END_SCREEN_OPENED { get; private set; } = "LEVEL_END_SCREEN_OPENED";
+
+
+    public static string GAME_STARTED { get; private set; } = "GAME_STARTED";
+    public static string LEVEL_RESTART { get; private set; } = "LEVEL_RESTART";
     public static string LEVEL_COMPLETE { get; private set; } = "LEVEL_COMPLETE";
     public static string LEVEL_FAILED { get; private set; } = "LEVEL_FAILED";
+    public static string LEVEL_PASS_DATA_COLLECTED { get; private set; } = "LEVEL_PASS_DATA_COLLECTED";
+    public static string GAME_ENDING { get; private set; } = "GAME_ENDING";
     public static string LEVEL_EXIT { get; private set; } = "LEVEL_EXIT";
 
     public static string ON_PAUSE_STATE_CHANGED { get; private set; } = "ON_PAUSE_STATE_CHANGED";
